4c5c1d3e2e6c3969b3e7051d7b2f282fc622fb56
Sai Gudigundla <sai@Sais-MacBook-Pro.local>
2018-01-22 20:08:45 -0500

    init now creates files and folders as needed

--- src/commands/init.js
+++ src/commands/init.js
@@ -4,7 +4,20 @@ exports.command = 'init'
 exports.desc = 'Initialize jit repo'
 exports.builder = {}
 exports.handler = function (argv) {
-	fs.mkdir('.jit', () => {
-        console.log('Initialized jit repo');
+	fs.mkdir('.jit', () => {        
+        fs.mkdirSync('.jit/refs');
+        fs.mkdirSync('.jit/refs/heads');
+        fs.mkdirSync('.jit/refs/tags');        
+        
+        fs.writeFileSync('.jit/HEAD', 'ref: refs/heads/master');
+        fs.writeFileSync('.jit/description', "Unnamed repository; edit this file 'description' to name the repository.");
+
+        fs.mkdirSync('.jit/objects');
+        
+        fs.mkdirSync('.jit/hooks');
+
+        fs.mkdirSync('.jit/info');        
+
+        console.log(`Initialized empty jit repo in ${process.cwd()}`);
     });
 }
