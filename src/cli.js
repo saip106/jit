53897842210090ebc0fefcb49270451d3dee54cd
Sai Gudigundla <sai@Sais-MacBook-Pro.local>
2018-01-23 21:15:51 -0500

    added status

--- src/cli.js
+++ src/cli.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 const yargs = require('yargs');
 
 exports.run = function() {
-        var cliArguments = yargs.argv._;
-        if (cliArguments.includes('init')) {
-            yargs
-                .commandDir('commands')
-                .demandCommand()
-                .help()
-                .argv
-        }
-        else if(!fs.existsSync(path.join(__dirname + '.jit'))) {
-            throw new Error('Not a jit repo');
-        }
+    var cliArguments = yargs.argv._;
+    if (cliArguments.includes('init')) {
+        yargs
+            .commandDir('commands')
+            .demandCommand()
+            .help()
+            .argv
     }
+    else if(!fs.existsSync(path.join(__dirname + '.jit'))) {
+        throw new Error('Not a jit repo');
+    }
+}
 
