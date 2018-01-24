5f9581fcee40d39daec18dd4fa85f2c08859949d
Sai Gudigundla <sai@Sais-MacBook-Pro.local>
2018-01-23 21:15:14 -0500

    added log code formatting

--- src/commands/log.js
+++ src/commands/log.js
@@ -1,1 +1,27 @@
 //fatal: your current branch 'master' does not have any commits yet
+const fs = require('fs');
+const path = require('path');
+
+exports.command = 'log'
+exports.desc = 'Show commit logs'
+exports.builder = {}
+exports.handler = function (argv) {
+    const rootJitDirectory = './.git';
+
+    //read HEAD file
+    const headFilePath = path.join(rootJitDirectory, HEAD);
+    fs.readFile(headFilePath, (error, data) => {
+        //ref: refs/heads/master
+        let parts = data.split(' ');
+        if (parts.length !== 2 || parts[0] == 'ref:') {
+            throw new Error('Invalid data in HEAD');
+        }
+        let headPointer = parts[1];
+
+        const headRefPath = path.join(rootJitDirectory, headPointer);
+        fs.readFile(headFilePath, (error, data) => {
+            
+        });
+    });
+    //traverse the tree from there
+}
